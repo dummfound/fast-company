7abfb9e4a21d6b72b01b199715ea52a70103d339
Gregdabro <gregmonak@gmail.com>
2022-08-02 18:54:28 +0200

    added import range

--- src/app/components/pagination.jsx
+++ src/app/components/pagination.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import _ from "lodash";
+import { range } from "lodash";
 import PropTypes from "prop-types";
 
 const Pagination = ({ itemsCount, pageSize, onPageChange, currentPage }) => {
     const pageCount = Math.ceil(itemsCount / pageSize);
     if (pageCount === 1) return null;
-    const pages = _.range(1, pageCount + 1);
+    const pages = range(1, pageCount + 1);
     return (
         <nav>
             <ul className="pagination">
