5cb6a000b0e4542f37c538145d3cc5f2b5bb0283
Gregdabro <gregmonak@gmail.com>
2022-08-23 14:58:09 +0200

    Controlled and uncontrolled components

--- src/app/components/login.jsx
+++ src/app/components/login.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const handleChange = (e) => {
+        setEmail(e.target.value);
+        console.log("e", e.target.value);
+    };
     return (
         <form>
             <div>
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email"/>
+                <input type="text" id="email" value={email} onChange={handleChange}/>
             </div>
             <div>
                 <label htmlFor="password">Password</label>
