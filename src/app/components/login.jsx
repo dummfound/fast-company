a148f135503870892281d825669c236633381161
Gregdabro <gregmonak@gmail.com>
2022-08-23 14:36:52 +0200

    created form

--- src/app/components/login.jsx
+++ src/app/components/login.jsx
@@ -2,9 +2,16 @@ import React from "react";
 
 const Login = () => {
     return (
-        <h1>
-            Login
-        </h1>
+        <form>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email"/>
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password"/>
+            </div>
+        </form>
     );
 };
 
