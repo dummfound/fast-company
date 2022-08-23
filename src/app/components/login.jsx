e30653c19a3ab2df0df1afaa17ce08f4dbb7cdbd
Gregdabro <gregmonak@gmail.com>
2022-08-23 17:40:18 +0200

    status Validation

--- src/app/components/login.jsx
+++ src/app/components/login.jsx
@@ -30,6 +30,16 @@ const Login = () => {
         password: {
             isRequired: {
                 message: "Password is required!"
+            },
+            isCapitalSymbol: {
+                message: "Password must contain a minimum of 1 upper case letter!"
+            },
+            isContainDigit: {
+                message: "Password must contain a minimum of 1 digit!"
+            },
+            min: {
+                message: "Passwords must be at least 8 characters!",
+                value: 8
             }
         }
     };
