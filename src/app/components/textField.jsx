c483f35be26bb2910275ff1f1fae64d704d6d523
Gregdabro <gregmonak@gmail.com>
2022-08-23 18:45:19 +0200

    Hide/Show password

--- src/app/components/textField.jsx
+++ src/app/components/textField.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const TextField = ({ label, type, name, value, onChange, error }) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const toggleShowPassword = () => {
+        setShowPassword((prevState) => !prevState);
+    };
+
     const getInputClasses = () => {
         return "form-control" + (error ? " is-invalid" : "");
     };
     return (
         <div className="mb-4">
             <label htmlFor={name}>{label}</label>
-            <input
-                type={type}
-                id={name}
-                name={name}
-                value={value}
-                onChange={onChange}
-                className={getInputClasses()}
-            />
-            {error && <div className="invalid-feedback">{error}</div>}
+            <div className="input-group has-validation">
+                <input
+
+                    // Если пароль показываем, то type === "text"
+                    type={showPassword ? "text" : type}
+                    id={name}
+                    value={value}
+                    onChange={onChange}
+                    name={name}
+                    className={getInputClasses()}
+                />
+                {type === "password" && (
+                    <button
+                        type="button"
+                        className="btn btn-outline-secondary"
+                        onClick={toggleShowPassword}
+                    >
+                        <i className={"bi bi-eye" + (showPassword ? "-slash" : "")}></i>
+                    </button>
+                )}
+                {error && <div className="invalid-feedback">{error}</div>}
+            </div>
         </div>
     );
 };
